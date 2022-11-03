9aa612b309e73cf3c44660dbed78fe7ce5eb411a
Lộc <locpham050100@gmail.com>
2022-11-03 10:53:53 +0700

    Right Menu UI Design

--- src/components/rightBar/RightBar.js
+++ src/components/rightBar/RightBar.js
@@ -2,7 +2,133 @@ import React from "react";
 import "./rightBar.scss";
 
 function RightBar() {
-  return <div className="rightBar">RightBar</div>;
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Đề xuất cho bạn</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <span>Elon Musk</span>
+            </div>
+            <div className="buttons">
+              <button>Theo dõi</button>
+              <button>Bỏ qua</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <span>Elon Musk</span>
+            </div>
+            <div className="buttons">
+              <button>Theo dõi</button>
+              <button>Bỏ qua</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Hoạt động gần đây</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <p>
+                <span>Elon Musk</span> đăng bài viết
+              </p>
+            </div>
+            <span className="time">1 phút trước</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <p>
+                <span>Elon Musk</span> đăng bài viết
+              </p>
+            </div>
+            <span className="time">1 phút trước</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <p>
+                <span>Elon Musk</span> đăng bài viết
+              </p>
+            </div>
+            <span className="time">1 phút trước</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Bạn bè trực tuyến</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <div className="online" />
+              <span>Elon Musk</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <div className="online" />
+              <span>Elon Musk</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <div className="online" />
+              <span>Elon Musk</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <div className="online" />
+              <span>Elon Musk</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://static.independent.co.uk/2022/08/21/10/newFile-1.jpg?quality=75&width=982&height=726&auto=webp"
+                alt=""
+              />
+              <div className="online" />
+              <span>Elon Musk</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightBar;
