f727f08b8285e4a138e76eb6938375371ee0ec9a
Lộc <locpham050100@gmail.com>
2022-11-15 18:07:27 +0700

    Create Dark Mode

--- src/App.js
+++ src/App.js
@@ -13,13 +13,14 @@ import {
 import Navbar from "./components/navbar/Navbar";
 import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
+import "./style.scss"
 
 function App() {
   const currentUser = true;
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
